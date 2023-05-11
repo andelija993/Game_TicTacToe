a733c00687bfa15a44d91c7abd420d9ecc5088a6
Andjelija Nesic <andelijanesic@gmail.com>
2023-05-11 16:10:01 +0200

    Game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+import Cell from "./components/Cell";
+
+const App = () => {
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", "",]);
+  const [go, setGo] = useState("circle");
+  const [winningMessage, setWinningMessage] = useState(null);
+
+  const message = "it is now " + go + " 's turn! "
+
+  const checkScore = () => {
+    const winningCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ];
+
+    winningCombos.forEach((arr) => {
+      let winningCirclePLayer = arr.every(cell => cells[cell] === "circle");
+      let winningCrossPLayer = arr.every(cell => cells[cell] === "cross");
+
+      if (winningCirclePLayer) {
+        setWinningMessage("Circle player wins!");
+        return;
+      } if (winningCrossPLayer) {
+        setWinningMessage("Cross player wins!");
+        return;
+      }
+
+    })
+  }
+
+  const handleReset = () => {
+    setCells(["", "", "", "", "", "", "", "", "",]);
+    setWinningMessage(null);
+    setGo("circle");
+  }
+
+  useEffect(() => {
+    checkScore();
+  }, [cells])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Tic Tac Toe</h1>
+      {winningMessage &&
+        <button onClick={handleReset}>New Game</button>}
+      <div className="gameboard">
+        {cells.map((cell, index) => <Cell
+          key={index}
+          id={index}
+          cell={cell}
+          setCells={setCells}
+          go={go}
+          setGo={setGo}
+          cells={cells}
+          winningMessage={winningMessage}
+        />)}
+        <p>{winningMessage || message}</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/Cell.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Cell = ({ id, cell, go, setGo, cells, setCells, winningMessage }) => {
+
+
+    const handleClick = (e) => {
+        const taken = e.target.firstChild.classList.contains("circle") ||
+            e.target.firstChild.classList.contains("cross");
+
+        if (!taken) {
+            if (go === "circle") {
+                e.target.firstChild.classList.add("circle");
+                handleCellChange("circle")
+                setGo("cross");
+            }
+            if (go === "cross") {
+                e.target.firstChild.classList.add("cross");
+                handleCellChange("cross")
+                setGo("circle");
+            }
+        }
+    };
+
+    const handleCellChange = (className) => {
+        const nextCells = cells.map((cell, index) => {
+            if (index === id) {
+                return className;
+            } else {
+                return cell;
+            }
+        })
+        setCells(nextCells);
+
+    }
+
+    return (
+        <div className='square' id={id} onClick={!winningMessage ? handleClick : undefined}>
+            <div className={cell}></div>
+        </div>
+    )
+}
+
+export default Cell;
